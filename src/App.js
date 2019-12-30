a5b07cae0f8ea8dbf9b5d0b2bad5e6a74fd69358
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-12-29 23:00:47 -0500

    navigation bar finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import './index.css'
 
+const theme = createMuiTheme({
+  typography: {
+   "fontFamily": "\"Roboto\", \"Helvetica\", \"Arial\", sans-serif"
+  }
+});
 
 function App() {
   return (
     <div className="App">
-        <h1>DroneGate</h1>
+      <ThemeProvider theme={theme}>
+        <NavigationBar />
+      </ThemeProvider>
     </div>
   );
 }
